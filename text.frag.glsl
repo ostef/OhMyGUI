3acddb72c8733eda88e0ad2037caf4e0c5b2dcf3
Stefan <oumansour.stefan@gmail.com>
2026-04-19 13:26:42 +0200

    Basic text node sizing

--- text.frag.glsl
+++ text.frag.glsl
@@ -13,12 +13,5 @@ void main() {
     float a = 1 - smoothstep(-aa, aa, d);
     a = clamp(a, 0, 1);
 
-    vec2 shadow_uv = clamp(in_uv - 0.003, in_uvs.xy, in_uvs.zw);
-    d = -(texture(u_texture, shadow_uv).x - 0.5);
-    aa = length(vec2(dFdx(d), dFdy(d)));
-    float b = 1 - smoothstep(-0.2, 0.2, d);
-    b = clamp(b, 0, 1);
-
-    out_color = vec4(0,0,0,b * 0.5);
-    out_color = mix(out_color, vec4(1,0,0,1), a);
+    out_color = vec4(0,0,0,a);
 }
