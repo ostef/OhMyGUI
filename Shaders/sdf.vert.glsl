742ceb9255a55895c8115450c0600e1c84bc187e
Stefan <oumansour.stefan@gmail.com>
2026-06-15 23:01:20 +0200

    Bring back SDF renderer!

--- Shaders/sdf.vert.glsl
+++ Shaders/sdf.vert.glsl
@@ -9,9 +9,21 @@ void main() {
     out_shape_drawing_index = gl_InstanceID + gl_BaseInstance;
     ShapeDrawing drawing = u_shape_drawings[out_shape_drawing_index];
 
+    vec4 bounds = drawing.bounds;
+
+    // Inflate bounds so we can draw the border
+    bounds.xy -= max(-drawing.border_size_and_inset.y, 0);
+    bounds.zw += max(-drawing.border_size_and_inset.y, 0);
+
+    // Inflate bounds so we can draw the outer shadow
+    bounds.xy -= max(drawing.outer_shadow_blur, 0);
+    bounds.zw += max(drawing.outer_shadow_blur, 0);
+    bounds.xy = min(bounds.xy, bounds.xy + drawing.outer_shadow_offset);
+    bounds.zw = max(bounds.zw, bounds.zw + drawing.outer_shadow_offset);
+
     const vec2 positions[] = vec2[](
-        vec2(drawing.bounds.z, drawing.bounds.w), vec2(drawing.bounds.x, drawing.bounds.w), vec2(drawing.bounds.x, drawing.bounds.y),
-        vec2(drawing.bounds.z, drawing.bounds.w), vec2(drawing.bounds.x, drawing.bounds.y), vec2(drawing.bounds.z, drawing.bounds.y)
+        vec2(bounds.z, bounds.w), vec2(bounds.x, bounds.w), vec2(bounds.x, bounds.y),
+        vec2(bounds.z, bounds.w), vec2(bounds.x, bounds.y), vec2(bounds.z, bounds.y)
     );
 
     gl_Position.xy = ((positions[gl_VertexID] + drawing.position) / u_viewport_size) * 2 - vec2(1);
